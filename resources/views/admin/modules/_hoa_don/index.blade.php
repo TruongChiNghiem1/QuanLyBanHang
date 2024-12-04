95fee693330b27692682cf081b601c1439baef80
TruongChiNghiem1 <nghiem0859774418@gmail.com>
2024-12-04 23:45:41 +0700

    sua lai view table

--- resources/views/admin/modules/_hoa_don/index.blade.php
+++ resources/views/admin/modules/_hoa_don/index.blade.php
@@ -3,13 +3,6 @@
 @section('content')
     <div class="col-lg-12">
         <div class="white_card card_height_100 mb_30">
-            <div class="white_card_header">
-                <div class="box_header m-0">
-                    <div class="main-title">
-                        <h3 class="m-0">Data table</h3>
-                    </div>
-                </div>
-            </div>
             <div class="white_card_body">
                 <div class="QA_section">
                     <div class="white_box_tittle list_header">
